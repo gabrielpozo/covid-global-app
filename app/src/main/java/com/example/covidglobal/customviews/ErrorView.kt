bca4ee5e485e4c8baa73aee7a25c1c44f3dea4e6
Sarantis Tofas <sarantis.tofas@mobgen.com>
2020-09-01 23:01:06 +0200

    [ADD] Error view

---
+++ app/src/main/java/com/example/covidglobal/customviews/ErrorView.kt
@@ -0,0 +1,19 @@
+package com.example.covidglobal.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+/**
+ * Created by Akis on 01/09/2020.
+ */
+class ErrorView : ConstraintLayout {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+}
---
+++ app/src/main/java/com/example/covidglobal/general/DateExtension.kt
@@ -0,0 +1,7 @@
+package com.example.covidglobal.general
+
+/**
+ * Created by Akis on 01/09/2020.
+ */
+class DateExtension {
+}
